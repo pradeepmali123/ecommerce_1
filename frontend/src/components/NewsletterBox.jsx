fe685253c012292012c5b85ead6d249472027c93
pradeepmali123 <pradeep.mali@sakec.ac.in>
2025-05-02 22:31:51 +0530

    changes in letterbox 2

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const NewsletterBox = () => {
+const NewsLetterBox = () => {
 
     const onSubmitHandler = (event) => {
         event.preventDefault();
@@ -21,4 +21,4 @@ const NewsletterBox = () => {
     )
 }
 
-export default NewsletterBox
+export default NewsLetterBox
