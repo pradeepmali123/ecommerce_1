6f82fcf087f0f63cdce0e243af17094bb6a0bdb9
pradeepmali123 <pradeep.mali@sakec.ac.in>
2025-05-03 11:32:59 +0530

    Fix CORS to allow frontend admin domain

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ connectCloudinary()
 
 //middleware 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: 'https://ecommerce-1-admin.vercel.app',
+    credentials: true
+}));
+
 
 //api endpoints 
 app.use('/api/user', userRouter)
